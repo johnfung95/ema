2d19027f4976d832ac7c39c633467c9096a8f060
John <59725906+johnfung95@users.noreply.github.com>
2025-11-13 15:17:16 +0800

    Adding backdrop to spinner

--- components/UI/Loading.tsx
+++ components/UI/Loading.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Spinner: React.FC = () => {
   return (
-    <div className="loader"></div>
+    <div className="fixed top-0 left-0 w-full h-full bg-black opacity-50 z-60 flex justify-center items-center">
+      <div className="loader"></div>
+    </div>
   );
 };
 
