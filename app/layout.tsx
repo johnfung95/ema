d9f4260b48292d686a5b2c81252d86561f4dfee5
John <59725906+johnfung95@users.noreply.github.com>
2025-11-13 14:40:14 +0800

    Try remove next/font/google for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,31 @@
-import '../styles/globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Background from '../components/UI/Background'
-import AuthProvider from '../components/Provider'
+import "../styles/globals.css";
+import type { Metadata } from "next";
+// import { Inter } from 'next/font/google'
+import Background from "../components/UI/Background";
+import AuthProvider from "../components/Provider";
 import { getServerSession } from "next-auth";
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'EMA - Wishes',
-  description: 'Write your own EMA',
-}
+  title: "EMA - Wishes",
+  description: "Write your own EMA",
+};
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  const session = await getServerSession()
+  const session = await getServerSession();
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <AuthProvider session={session}>
-          <Background>
-            {children}
-          </Background>
+          <Background>{children}</Background>
         </AuthProvider>
       </body>
     </html>
-  )
+  );
 }
